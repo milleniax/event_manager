447511cd877075646a1d685820d3ac92484e7cf1
milleniax <marsel.abdullin.00@mail.ru>
2020-07-14 01:14:43 +0300

    Form.vue fix

--- frontend/src/components/Form.vue
+++ frontend/src/components/Form.vue
@@ -2,10 +2,10 @@
     <form @submit.prevent="submitForm">
         <div class="form-group row">
             <div class="col-10 d-flex">
-                <input type="" class="title form-control" v-model="event.title" placeholder="Название">
-                <input type="" class="content form-control" v-model="event.content" placeholder="Содержание">
-                <input type="" class="event_date form-control" v-model="event.event_date" placeholder="Дата">
-                <input type="" class="email form-control" v-model="event.email" placeholder="Email">
+                <input type="" class="title form-control" v-model="getEvent.title" placeholder="Название">
+                <input type="" class="content form-control" v-model="getEvent.content" placeholder="Содержание">
+                <input type="" class="event_date form-control" v-model="getEvent.event_date" placeholder="Дата">
+                <input type="" class="email form-control" v-model="getEvent.email" placeholder="Email">
             </div>
             <div class="d-flex flex-column">
                 <button class="btn btn-success mt-auto" >Создать</button>
